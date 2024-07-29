9b6787214e2dbd97b7904cf5fd8c398dea869b3a
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2024-07-29 18:33:31 +0530

    Time: 1665 ms (11.22%) | Memory: 19.1 MB (48.64%) - LeetSync

---
+++ 136-single-number/single-number.py
@@ -0,0 +1,21 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        lst=[]
+        e=[]
+        for i in nums:
+            if i not in lst:
+                lst.append(i)
+            else:
+                e.append(i)
+        for i in lst:
+            if i not in e:
+                return i
+
+        
+        
+        
+        
+        # hash=Counter(nums)
+        # for key,value in hash.items():
+        #     if value==1:
+        #         return key
