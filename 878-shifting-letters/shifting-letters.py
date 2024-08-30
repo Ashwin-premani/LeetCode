cf000cf3be4af3d52de35202c97230cabbb2e0fd
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2024-08-31 01:04:02 +0530

    Time: 542 ms (94.63%) | Memory: 30 MB (69.01%) - LeetSync

--- 878-shifting-letters/shifting-letters.py
+++ 878-shifting-letters/shifting-letters.py
@@ -1,12 +1,7 @@
-from typing import List
-from itertools import accumulate
-
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
-        s = list(s)
-        shifts = list(reversed(list(accumulate(reversed(shifts)))))
-        
-        for i, sh in enumerate(shifts):
-            s[i] = chr(((ord(s[i]) - ord('a') + sh) % 26) + ord('a'))
-        
+        s=list(s)
+        shifts = reversed(list(accumulate(list(reversed(shifts)))))
+        for i,sh in enumerate(shifts):
+            s[i] = chr((((ord(s[i])-ord('a'))+sh)%26)+97)
         return "".join(s)
