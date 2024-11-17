eb2a324c8190ad0817ac21f238be67a06e75fad9
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2024-11-17 21:45:32 +0530

    Time: 402 ms (15.01%) | Memory: 36.8 MB (9.94%) - LeetSync

---
+++ 892-shortest-subarray-with-sum-at-least-k/shortest-subarray-with-sum-at-least-k.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shortestSubarray(self, nums: List[int], k: int) -> int:
+        res = float("inf")
+
+        cur_sum = 0
+        min_heap = [] # (prefix, end_idx)
+
+        for r in range(len(nums)):
+            cur_sum += nums[r]
+
+            if cur_sum >=k:
+                res = min(res,r+1)
+
+            #min window ending at r
+            while min_heap and cur_sum -min_heap[0][0] >= k:
+                prefix, end_idx = heapq.heappop(min_heap)
+                res = min(res, r - end_idx)
+            
+            heapq.heappush(min_heap,(cur_sum,r))
+
+
+
+        return -1 if res == float("inf") else res
