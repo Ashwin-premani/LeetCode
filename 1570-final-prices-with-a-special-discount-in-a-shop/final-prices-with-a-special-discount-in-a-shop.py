bb7f93f3ec2347d8ea8214fb798c85482d8cf61f
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2024-12-18 23:25:13 +0530

    Time: 3 ms (55.13%) | Memory: 18 MB (10.22%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,9 +1,23 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        j = 0
+        # Monotonic increasing stack O(n)
+        # intution is to pop elemets whenever smaller element is encountere 
+        # till that element is larger that element below it
+        stack = [] # Monotonic stack (index (value in increasing order))
         for i in range(len(prices)):
-            for j in range(i + 1, len(prices)):
-                if prices[j] <= prices[i]:
-                    prices[i] -= prices[j]
-                    break
+            while stack and prices[stack[-1]] >= prices[i]:
+                j = stack.pop()
+                prices[j] -= prices[i]
+            stack.append(i)
+            
         return prices
+
+
+        # brute force O(n^2)
+        # j = 0
+        # for i in range(len(prices)):
+        #     for j in range(i + 1, len(prices)):
+        #         if prices[j] <= prices[i]:
+        #             prices[i] -= prices[j]
+        #             break
+        # return prices
