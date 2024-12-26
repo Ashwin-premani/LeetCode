87f2f06167b99338896fdadd16379f19a3984978
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2024-12-27 00:02:03 +0530

    Time: 130 ms (73.51%) | Memory: 18.5 MB (58.81%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,24 +1,35 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        # optimized bottom up
+        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
 
-        # backtrack using hash
-        dp = {} #(index, cur_sum)
-        def backtrack(i, cur_sum):
-            if (i, cur_sum) in dp:
-                return dp[(i, cur_sum)]
-            if i == len(nums):
-                return 1 if cur_sum == target else 0
+        dp[0][0] = 1 # (elements, sum) -> 1 way (1 way to sum to zero with first 0 elements)
 
-            dp[(i, cur_sum)] =  (
-                backtrack(i + 1, cur_sum + nums[i]) +
-                backtrack(i + 1, cur_sum - nums[i])
-            )
-            return dp[(i, cur_sum)]
-        return backtrack(0, 0)
+        for i in range(len(nums)):
+            for cur_sum, count in dp[i].items():
+                dp[i + 1][cur_sum + nums[i]] += count
+                dp[i + 1][cur_sum - nums[i]] += count
+        return dp[len(nums)][target]
 
 
+        # backtrack using hash O(n * m)
+        # dp = {} #(index, cur_sum)
+        # def backtrack(i, cur_sum):
+        #     if (i, cur_sum) in dp:
+        #         return dp[(i, cur_sum)]
+        #     if i == len(nums):
+        #         return 1 if cur_sum == target else 0
 
-        # brute force backtrack 2^n
+        #     dp[(i, cur_sum)] =  (
+        #         backtrack(i + 1, cur_sum + nums[i]) +
+        #         backtrack(i + 1, cur_sum - nums[i])
+        #     )
+        #     return dp[(i, cur_sum)]
+        # return backtrack(0, 0)
+
+
+
+        # brute force backtrack O(2^n) (timeout)
         # def backtrack(i, cur_sum):
         #     if i == len(nums):
         #         return 1 if cur_sum == target else 0
