b1657cb51f938e3c0a526e826806fd8ccb20540e
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2024-12-31 23:50:52 +0530

    Time: 9 ms (23.01%) | Memory: 17.8 MB (28.23%) - LeetSync

--- 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
+++ 1025-minimum-cost-for-tickets/minimum-cost-for-tickets.py
@@ -1,21 +1,18 @@
 class Solution:
     def mincostTickets(self, days: List[int], costs: List[int]) -> int:
-        dp = {}
+        dp = { len(days) : 0}
 
         def backtrack(i):
-            if i == len(days):
-                return 0
             if i in dp:
                 return dp[i]
             
-            res = float('inf')
+            dp[i] = float('inf')
             j = i
             for c, d in zip(costs, [1, 7, 30]):
                 while j < len(days) and days[j] < days[i] + d:
                     j += 1
-                res = min(res, c + backtrack(j))
-            dp[i] = res
-            return res
+                dp[i] = min(dp[i], c + backtrack(j))
+            return dp[i]
         return backtrack(0)
                 
 
