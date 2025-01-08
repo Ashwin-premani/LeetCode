7a9d2613dbad54c396dd62198218067655ed9a17
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-01-08 15:48:06 +0530

    Time: 11 ms (56.86%) | Memory: 17.7 MB (23.73%) - LeetSync

--- 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
+++ 3309-count-prefix-and-suffix-pairs-i/count-prefix-and-suffix-pairs-i.py
@@ -1,9 +1,46 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+    
+    def add(self, w):
+        cur = self.root
+
+        for c1, c2 in zip(w, reversed(w)):
+            if (c1, c2) not in cur.children:
+                cur.children[(c1, c2)] = TrieNode()
+            cur = cur.children[(c1, c2)]
+            cur.count += 1
+    def count(self, w):
+        cur = self.root
+
+        for c1, c2 in zip(w, reversed(w)):
+            if (c1, c2) not in cur.children:
+                return 0
+            cur = cur.children[(c1, c2)]
+        return cur.count
+
 class Solution:
     def countPrefixSuffixPairs(self, words: List[str]) -> int:
         res = 0
-        for i in range(len(words)):
-            for j in range(i + 1, len(words)):
-                L = len(words[i])
-                if words[i] == words[j][:L] and words[i] == words[j][-L:]:
-                    res += 1
+        root = Trie()
+
+        for w in reversed(words):
+            res += root.count(w)
+            root.add(w)
         return res
+
+# class Solution:
+#     def countPrefixSuffixPairs(self, words: List[str]) -> int:
+#         res = 0
+#         for i in range(len(words)):
+#             for j in range(i + 1, len(words)):
+#                 L = len(words[i])
+#                 if words[j].startswith(words[i]) and words[j].endswith(words[i]):
+#                     res += 1
+#                 # if words[i] == words[j][:L] and words[i] == words[j][-L:]:
+#                 #     res += 1
+#         return res
