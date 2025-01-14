427f1e9fac27c9123924449630f00c8dcc6a32af
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-01-14 11:41:01 +0530

    Time: 18 ms (40.10%) | Memory: 17.6 MB (45.05%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.py
@@ -1,11 +1,46 @@
 class Solution:
     def findThePrefixCommonArray(self, A: List[int], B: List[int]) -> List[int]:
-        C = collections.Counter()
-
         ans = []
+        count = 0
+        c = collections.Counter()
         for x, y in zip(A, B):
-            C[x] += 1
-            C[y] += 1
-
-            ans.append(sum(1 for x in C.keys() if C[x] == 2))
+            c[x] -= 1
+            c[y] += 1
+            if c[x] == 0:
+                count += 1
+            if c[y] == 0:
+                count += 1
+            if x == y:
+                count -= 1
+            ans.append(count)
         return ans
+
+        # n = len(A)
+        
+        # C = [0] * n
+        # seta, setb = set(), set()
+        # for i in range(n):
+        #     C[i] = C[i - 1]
+        #     if A[i] == B[i]:
+        #         C[i] += 1
+        #     else:
+        #         if A[i] in setb:
+        #             C[i] += 1
+        #         if B[i] in seta:
+        #             C[i] += 1
+        #         seta.add(A[i])
+        #         setb.add(B[i])
+        # return C
+
+
+
+
+        # C = collections.Counter()
+
+        # ans = []
+        # for x, y in zip(A, B):
+        #     C[x] += 1
+        #     C[y] += 1
+
+        #     ans.append(sum(1 for x in C.keys() if C[x] == 2))
+        # return ans
