31e321be143c898c6cb9916ac93498bff08d7140
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-01-23 01:36:17 +0530

    Time: 1599 ms (28.53%) | Memory: 221.7 MB (22.22%) - LeetSync

---
+++ 1876-map-of-highest-peak/map-of-highest-peak.py
@@ -0,0 +1,28 @@
+class Solution:
+    def highestPeak(self, isWater: List[List[int]]) -> List[List[int]]:
+        rows, cols = len(isWater), len(isWater[0])
+        q = deque() # (r, c)
+        visit = set()
+        res = [[-1] * cols for _ in range(rows)]
+        # enqueue all water cells
+        for r in range(rows):
+            for c in range(cols):
+                if isWater[r][c]:
+                    q.append((r, c))
+                    visit.add((r, c))
+                    res[r][c] = 0
+            
+        # BFS
+        while q:
+            r, c = q.popleft()
+            h = res[r][c]
+
+            neighbors = [[r+1, c], [r,c + 1], [r - 1, c],[r, c-1]]
+            for nr, nc in neighbors:
+                if nr < 0 or nc < 0 or nr == rows or nc == cols or (nr, nc) in visit:
+                    continue
+                q.append((nr, nc))
+                visit.add((nr, nc))
+                res[nr][nc] = h + 1
+        return res
+
