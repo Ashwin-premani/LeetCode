4f77ab32a8c5abdc99951f576923a3a43b2a2483
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-02-06 22:56:24 +0530

    Time: 414 ms (38.86%) | Memory: 46.5 MB (41.71%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.py
+++ 1364-tuple-with-same-product/tuple-with-same-product.py
@@ -1,14 +1,28 @@
 class Solution:
     def tupleSameProduct(self, nums: List[int]) -> int:
-        pair = defaultdict(int)
+        # using math and 1 hash
         prod = defaultdict(int)
 
         for i in range(len(nums)):
             for j in range(i + 1, len(nums)):
                 p = nums[i] * nums[j]
-                pair[p] += prod[p]
                 prod[p] += 1
+
         res = 0
-        for v in pair.values():
-            res += 8 * v
+        for v in prod.values():
+            res += 8 * (v * (v - 1)) // 2
         return res
+
+        # using hash(2nd)
+        # pair = defaultdict(int)
+        # prod = defaultdict(int)
+
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         p = nums[i] * nums[j]
+        #         pair[p] += prod[p]
+        #         prod[p] += 1
+        # res = 0
+        # for v in pair.values():
+        #     res += 8 * v
+        # return res
