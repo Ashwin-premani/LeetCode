eae930f548602b987e32822790420a6056915223
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-02-28 01:12:29 +0530

    Time: 1140 ms (42.45%) | Memory: 25.6 MB (19.61%) - LeetSync

--- 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
+++ 905-length-of-longest-fibonacci-subsequence/length-of-longest-fibonacci-subsequence.py
@@ -1,16 +1,32 @@
 class Solution:
     def lenLongestFibSubseq(self, arr: List[int]) -> int:
-        arr_set = set(arr)
+        #DP
+        arr_map = {n:i for i,n in enumerate(arr)}
         res = 0
-
-        for i in range(len(arr)-1):
-            for j in range(i+1, len(arr)):
+        dp = [[0] * len(arr) for _ in range(len(arr))]
+        for i in reversed(range(len(arr)-1)):
+            for j in reversed(range(i+1, len(arr))):
                 prev, cur = arr[i], arr[j]
                 nxt = prev + cur
                 l = 2
-                while nxt in arr_set:
-                    l += 1
-                    prev, cur = cur, nxt
-                    nxt = prev + cur
+                if nxt in arr_map:
+                    l = 1 + dp[j][arr_map[nxt]]
                     res = max(res, l)
+
+                dp[i][j] = l
         return res
+
+        # linear
+        # arr_set = set(arr)
+        # res = 0
+        # for i in range(len(arr)-1):
+        #     for j in range(i+1, len(arr)):
+        #         prev, cur = arr[i], arr[j]
+        #         nxt = prev + cur
+        #         l = 2
+        #         while nxt in arr_set:
+        #             l += 1
+        #             prev, cur = cur, nxt
+        #             nxt = prev + cur
+        #             res = max(res, l)
+        # return res
