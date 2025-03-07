f6d64b619e175fe89dfcff43ae374771bc9e70b6
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-07 21:29:53 +0530

    Time: 1937 ms (33.44%) | Memory: 35 MB (26.56%) - LeetSync

--- 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
+++ 2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.py
@@ -1,34 +1,56 @@
-import math
-from typing import List
-
 class Solution:
     def closestPrimes(self, left: int, right: int) -> List[int]:
-        def isprime(n):
-            if n < 2:
-                return False
-            if n == 2:
-                return True
-            if n % 2 == 0:
-                return False
-            for i in range(3, int(math.sqrt(n)) + 1, 2):
-                if n % i == 0:
-                    return False
-            return True
+        # sieve of eratosthenes
+        def get_primes():
+            is_prime = [True] * (right + 1)
+            is_prime[0] = is_prime[1] = False
+
+            for n in range(2, int(sqrt(right)) + 1):
+                if not is_prime[n]:
+                    continue
+                for m in range(n+n, right + 1, n):
+                    is_prime[m] = False
+            primes = []
+            for i in range(len(is_prime)):
+                if is_prime[i] and i >= left:
+                    primes.append(i)
+            return primes
+
+        primes = get_primes()
+        res = [-1, -1]
+        diff = right - left + 1
+        for i in range(1, len(primes)):
+            if primes[i] - primes[i - 1] < diff:
+                diff = primes[i] - primes[i - 1]
+                res = [primes[i - 1], primes[i]]
+        return res
+        # brute force
+        # def isprime(n):
+        #     if n < 2:
+        #         return False
+        #     if n == 2:
+        #         return True
+        #     if n % 2 == 0:
+        #         return False
+        #     for i in range(3, int(math.sqrt(n)) + 1, 2):
+        #         if n % i == 0:
+        #             return False
+        #     return True
         
-        res = []
-        for i in range(left, right+1):
-            if isprime(i):
-                res.append(i)
+        # res = []
+        # for i in range(left, right+1):
+        #     if isprime(i):
+        #         res.append(i)
         
-        n1 = -1
-        n2 = -1
-        m = float('inf')
+        # n1 = -1
+        # n2 = -1
+        # m = float('inf')
         
-        for i in range(len(res) - 1):
-            current_gap = res[i+1] - res[i]
-            if current_gap < m:  # Changed from '>' to '<' and using strictly less than
-                m = current_gap
-                n1 = res[i]
-                n2 = res[i+1]
+        # for i in range(len(res) - 1):
+        #     current_gap = res[i+1] - res[i]
+        #     if current_gap < m:  # Changed from '>' to '<' and using strictly less than
+        #         m = current_gap
+        #         n1 = res[i]
+        #         n2 = res[i+1]
         
-        return [n1, n2]
+        # return [n1, n2]
