6229db0064a177873be9a1fce59f44e2f2ac5a55
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-09 12:06:51 +0530

    Time: 51 ms (79.64%) | Memory: 18 MB (14.33%) - LeetSync

--- 3463-alternating-groups-i/alternating-groups-i.py
+++ 3463-alternating-groups-i/alternating-groups-i.py
@@ -1,11 +1,9 @@
 class Solution:
     def numberOfAlternatingGroups(self, colors: List[int]) -> int:
         n = len(colors)
-        if n < 3:
-            return 0
+        colors += colors
         count = 0
-        window = colors + colors[:2]
         for i in range(n):
-            if window[i+1] != window[i] and window[i+1] != window[i+2]:
+            if colors[i] == colors[i+2] and colors[i] != colors[i+1]:
                 count += 1
         return count
