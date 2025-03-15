e926171185466f8c30d94aafaa2d9ddb26fd3dd9
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-15 15:37:52 +0530

    Time: 8 ms (70.98%) | Memory: 18.3 MB (80.61%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.py
+++ 152-maximum-product-subarray/maximum-product-subarray.py
@@ -1,23 +1,38 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        p = 1
-        ans = 0
-        for i in nums:
-            if i != 0:
-                p *= i
-                ans = max(ans,p)
-            else:
-                p = 1
-        p = 1
-        for i in reversed(nums):
-            if i != 0:
-                p *= i
-                ans = max(ans,p)
-            else:
-                p = 1
+        pre, suff = 1, 1
+        ans = min(nums)
+        n = len(nums)
+        for i in range(n):
+            if pre == 0:
+                pre = 1
+            if suff == 0:
+                suff = 1
+            pre = pre * nums[i]
+            suff = suff * nums[n - i - 1]
+            ans = max(pre, suff, ans)
         return ans
+        
+        # if len(nums) == 1:
+        #     return nums[0]
+        # p = 1
+        # ans = 0
+        # for i in nums:
+        #     if i != 0:
+        #         p *= i
+        #         ans = max(ans,p)
+        #     else:
+        #         p = 1
+        # p = 1
+        # for i in reversed(nums):
+        #     if i != 0:
+        #         p *= i
+        #         ans = max(ans,p)
+        #     else:
+        #         p = 1
+        # return ans
+
+
         # # Brute Force
         # if not nums:
         #     return 0
