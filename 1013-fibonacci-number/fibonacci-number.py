c19f4dba31483e354b39dc9be381b4747249535d
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-15 16:29:44 +0530

    Time: 37 ms (72.15%) | Memory: 17.6 MB (89.19%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,8 +1,17 @@
 class Solution:
     def fib(self, n: int) -> int:
-        
+        memo = {}
+
+        def helper(n):
+            if n in memo:
+                return memo[n]
+            if n <= 1:
+                return n
+            memo[n] = helper(n-1) + helper(n-2)
+            return memo[n]
+        return helper(n)
 
         # recursion
-        if n <= 1:
-            return n
-        return self.fib(n-1) + self.fib(n-2)
+        # if n <= 1:
+        #     return n
+        # return self.fib(n-1) + self.fib(n-2)
