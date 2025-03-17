3632ae7a6dd12905c8692cd95d26601548567c24
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-17 11:24:06 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (91.41%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.py
@@ -1,12 +1,20 @@
 class Solution:
     def divideArray(self, nums: List[int]) -> bool:
-        # hash = Counter(nums)
-        count = {}
+        odd = set()
         for n in nums:
-            if n not in count:
-                count[n] = 0
-            count[n] += 1
-        for i,v in count.items():
-            if v % 2 != 0:
-                return False
-        return True
+            if n not in odd:
+                odd.add(n)
+            else:
+                odd.remove(n)
+        return False if odd else True
+
+        ## hash = Counter(nums)
+        # count = {}
+        # for n in nums:
+        #     if n not in count:
+        #         count[n] = 0
+        #     count[n] += 1
+        # for i,v in count.items():
+        #     if v % 2 != 0:
+        #         return False
+        # return True
