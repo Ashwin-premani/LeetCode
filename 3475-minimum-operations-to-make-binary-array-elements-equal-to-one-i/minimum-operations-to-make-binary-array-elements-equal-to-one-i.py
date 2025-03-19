f41d6207f17da09db5ecd3215edbf134f4b4da90
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-19 19:35:57 +0530

    Time: 76 ms (97.43%) | Memory: 21.5 MB (96.40%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.py
@@ -1,15 +1,24 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
-        def flip(nums, i):
-            nums[i] = 0 if nums[i] else 1
-        
         res = 0
-        for i in range(len(nums) - 2):
+        for i in range(len(nums)-2):
             if nums[i] == 0:
-                flip(nums, i)
-                flip(nums, i+1)
-                flip(nums, i+2)
                 res += 1
-        if not nums[-1] or not nums[-2]:
-            return -1
-        return res
+                nums[i + 1] = 1 - nums[i + 1]
+                nums[i + 2] = 1 - nums[i + 2]
+        
+        return res if nums[-1] == 1 and nums[-2] == 1 else -1
+
+
+        # def flip(nums, i):
+        #     nums[i] = 0 if nums[i] else 1
+        # res = 0
+        # for i in range(len(nums) - 2):
+        #     if nums[i] == 0:
+        #         flip(nums, i)
+        #         flip(nums, i+1)
+        #         flip(nums, i+2)
+        #         res += 1
+        # if not nums[-1] or not nums[-2]:
+        #     return -1
+        # return res
