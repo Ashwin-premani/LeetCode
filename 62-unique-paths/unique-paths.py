4be9eebb49ecaefee4f8ba533aace7ba4f9a9d0b
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-24 16:04:13 +0530

    Time: 3 ms (22.20%) | Memory: 17.8 MB (65.22%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,19 +1,33 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # tabulation
-        dp = [[-1] * n for _ in range(m)]
-        dp[0][0] = 1
-        
+        # tabulation space optimization
+        dp = [0 for i in range(n)]
         for i in range(m):
+            temp = [0 for _ in range(n)]
             for j in range(n):
                 if i == 0 and j == 0:
-                    dp[0][0] = 1  # Base case (start point)
+                    temp[0] = 1  # Base case (start point)
                 else:
-                    up = dp[i - 1][j] if i > 0 else 0
-                    left = dp[i][j - 1] if j > 0 else 0
-                    dp[i][j] = up + left
+                    temp[j] = dp[j] + temp[j - 1]
+            dp = temp
         
-        return dp[m - 1][n - 1]
+        return temp[n - 1]
+
+
+        # tabulation O(nxm)
+        # dp = [[-1] * n for _ in range(m)]
+        # dp[0][0] = 1
+        
+        # for i in range(m):
+        #     for j in range(n):
+        #         if i == 0 and j == 0:
+        #             dp[0][0] = 1  # Base case (start point)
+        #         else:
+        #             up = dp[i - 1][j] if i > 0 else 0
+        #             left = dp[i][j - 1] if j > 0 else 0
+        #             dp[i][j] = up + left
+        
+        # return dp[m - 1][n - 1]
 
 
         # memoization
