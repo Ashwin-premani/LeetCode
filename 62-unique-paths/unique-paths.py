2c0bf83bd4867fca123fe0e1de4f50bafdd46ce7
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-24 15:57:53 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (65.22%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,20 +1,36 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        # memoization
+        # tabulation
         dp = [[-1] * n for _ in range(m)]
+        dp[0][0] = 1
+        
+        for i in range(m):
+            for j in range(n):
+                if i == 0 and j == 0:
+                    dp[0][0] = 1  # Base case (start point)
+                else:
+                    up = dp[i - 1][j] if i > 0 else 0
+                    left = dp[i][j - 1] if j > 0 else 0
+                    dp[i][j] = up + left
+        
+        return dp[m - 1][n - 1]
 
-        def helper(i, j):
-            if i == 0 and j == 0:
-                return 1
-            if i < 0 or j < 0:
-                return 0
-            if dp[i][j] != -1:
-                return dp[i][j]
-            left = helper(i - 1, j)
-            up = helper(i, j - 1)
-            dp[i][j] = up + left
-            return dp[i][j]
-        return helper(m-1, n-1)
+
+        # memoization
+        # dp = [[-1] * n for _ in range(m)]
+
+        # def helper(i, j):
+        #     if i == 0 and j == 0:
+        #         return 1
+        #     if i < 0 or j < 0:
+        #         return 0
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     left = helper(i - 1, j)
+        #     up = helper(i, j - 1)
+        #     dp[i][j] = up + left
+        #     return dp[i][j]
+        # return helper(m-1, n-1)
 
         # def helper(i, j):
         #     if i == 0 and j == 0:
