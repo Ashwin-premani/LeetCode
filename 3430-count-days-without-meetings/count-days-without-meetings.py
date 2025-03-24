709ddd264cfd401b2d92332d998f88338a40cfcd
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-24 12:43:48 +0530

    Time: 154 ms (73.30%) | Memory: 52.9 MB (30.73%) - LeetSync

---
+++ 3430-count-days-without-meetings/count-days-without-meetings.py
@@ -0,0 +1,37 @@
+class Solution:
+    def countDays(self, days: int, meetings: List[List[int]]) -> int:
+        meetings.sort()
+        prev_start, prev_end = -1, -1
+        occupied = 0
+        for s, e in meetings:
+            if s > prev_end:
+                occupied += e - s + 1
+                prev_start, prev_end = s, e
+            else:
+                if e > prev_end:
+                    occupied += e - prev_end
+                    prev_end = e
+        return days - occupied 
+
+
+        # Passed TLE but Memory Limit exceeded
+        # meetings.sort()  
+        # res = set() 
+        # prev = 0
+
+        # for s, e in meetings:
+        #     if s > prev:
+        #         res.update(range(s, e + 1))
+        #     else:
+        #         res.update(range(prev, e + 1))
+        #     prev = max(prev, e)  
+
+        # return days - len(res)  
+
+
+        # Time Limit Exceeded
+        # res = set(range(1, days+1))
+        # for s, e in meetings:
+        #     for i in range(s, e+1):
+        #         res.discard(i)
+        # return len(res)
