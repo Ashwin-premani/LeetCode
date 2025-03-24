0ccbaf1bae1a19764cc5ec07e1d2474818881707
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-24 13:07:00 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.41%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,8 +1,32 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        r1, r2 = 0, 0
-        for i in nums:
-            temp = max(r1+i, r2)
-            r1=r2
-            r2=temp
-        return r2
+        n = len(nums)
+        
+        if n == 0:
+            return 0
+        if n == 1:
+            return nums[0]
+
+        def helper(temp):
+            prev = temp[0]
+            prev2 = 0
+            for i in range(1, len(temp)):
+                take = temp[i]
+                if i > 1:
+                    take += prev2
+                not_take = prev
+                cur = max(take, not_take)
+                prev2 = prev
+                prev = cur
+            return prev
+        return helper(nums)
+
+
+        
+
+        # r1, r2 = 0, 0
+        # for i in nums:
+        #     temp = max(r1+i, r2)
+        #     r1=r2
+        #     r2=temp
+        # return r2
