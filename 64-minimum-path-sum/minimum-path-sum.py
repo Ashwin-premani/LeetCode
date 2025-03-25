85eabf6593c601d21b1619a5707238102f43e5e2
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-25 15:39:23 +0530

    Time: 18 ms (38.82%) | Memory: 20.2 MB (50.18%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.py
+++ 64-minimum-path-sum/minimum-path-sum.py
@@ -1,29 +1,44 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
 
-        # Memoization
+        # Tabulation
         n, m = len(grid), len(grid[0])
-        dp = [[-1] * m for _ in range(n)]
-        def helper(i, j):
-            if  i == 0 and j == 0:
-                return grid[0][0]
-            if  i < 0 or j < 0:
-                return float('inf')
-            if dp[i][j] != -1:
-                return dp[i][j]
-            left = grid[i][j] + helper(i - 1, j)
-            up = grid[i][j] + helper(i, j - 1)
-            dp[i][j] = min(left, up) 
-            return dp[i][j]
-        return helper(len(grid) - 1, len(grid[0]) - 1)
+        dp = [[0] * m for _ in range(n)]
+        
+        for i in range(n):
+            for j in range(m):
+                if i == 0 and j == 0:
+                    dp[i][j] = grid[i][j]
+                else:
+                    left = dp[i-1][j] if i > 0 else float('inf')
+                    up = dp[i][j-1] if j > 0 else float('inf')
+                    dp[i][j] = grid[i][j] + min(left, up)
+        
+        return dp[n-1][m-1]
 
-        # Backtrack
-        def helper(i, j):
-            if  i == 0 and j == 0:
-                return grid[0][0]
-            if  i < 0 or j < 0:
-                return float('inf')
-            left = grid[i][j] + helper(i - 1, j)
-            up = grid[i][j] + helper(i, j - 1)
-            return min(left, up)
-        return helper(len(grid) - 1, len(grid[0]) - 1)
+        # # Memoization
+        # n, m = len(grid), len(grid[0])
+        # dp = [[-1] * m for _ in range(n)]
+        # def helper(i, j):
+        #     if  i == 0 and j == 0:
+        #         return grid[0][0]
+        #     if  i < 0 or j < 0:
+        #         return float('inf')
+        #     if dp[i][j] != -1:
+        #         return dp[i][j]
+        #     left = grid[i][j] + helper(i - 1, j)
+        #     up = grid[i][j] + helper(i, j - 1)
+        #     dp[i][j] = min(left, up) 
+        #     return dp[i][j]
+        # return helper(len(grid) - 1, len(grid[0]) - 1)
+
+        # # Backtrack
+        # def helper(i, j):
+        #     if  i == 0 and j == 0:
+        #         return grid[0][0]
+        #     if  i < 0 or j < 0:
+        #         return float('inf')
+        #     left = grid[i][j] + helper(i - 1, j)
+        #     up = grid[i][j] + helper(i, j - 1)
+        #     return min(left, up)
+        # return helper(len(grid) - 1, len(grid[0]) - 1)
