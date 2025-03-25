ebf338efabb20468d9155221a458afdb2db8fdee
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-25 15:10:21 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (11.44%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.py
+++ 63-unique-paths-ii/unique-paths-ii.py
@@ -1,5 +1,23 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
+        # Tabulation with space optimization
+        m, n = len(obstacleGrid), len(obstacleGrid[0])
+        dp = [0 for i in range(n)]
+        if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1:
+            return 0
+        for i in range(m):
+            temp = [0 for _ in range(n)]
+            for j in range(n):
+                if i == 0 and j == 0:
+                    temp[j] = 1  # Base case (start point)
+                elif  obstacleGrid[i][j] == 1:
+                    continue
+                else:
+                    temp[j] = dp[j] + temp[j - 1]
+            dp = temp
+        
+        return temp[n - 1]
+
         # Tabulation
         m, n = len(obstacleGrid), len(obstacleGrid[0])
         if obstacleGrid[0][0] == 1 or obstacleGrid[m-1][n-1] == 1:
