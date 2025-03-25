8e97bb3669c94ccca8659c7626ff9506c65ca12d
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-25 16:27:27 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (56.03%) - LeetSync

--- 120-triangle/triangle.py
+++ 120-triangle/triangle.py
@@ -1,5 +1,15 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # Optimized tabulation O(1)
+        n = len(triangle)
+
+        # Start from the second-last row and move upwards
+        for i in range(n - 2, -1, -1):
+            for j in range(len(triangle[i])):
+                triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+
+        return triangle[0][0]
+
         # Tabulation
         n = len(triangle)
         dp = [row[:] for row in triangle]  
