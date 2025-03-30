8be3f8112995e0118fdd16f1829248b991e4cefc
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-30 20:43:59 +0530

    Time: 5 ms (54.86%) | Memory: 17.9 MB (36.45%) - LeetSync

--- 768-partition-labels/partition-labels.py
+++ 768-partition-labels/partition-labels.py
@@ -4,13 +4,13 @@ class Solution:
         for i in range(len(s)):
             hash[s[i]] = i
         res = []
-        start = 0
+        size = 0
         end = 0
         l = 0
         for i, char in enumerate(s):
             end = max(end, hash[char])  
+            size += 1 
             if i == end:
-                res.append(i - start + 1)  
-                start = i + 1   
+                res.append(size)  
+                size = 0
         return res
-            
