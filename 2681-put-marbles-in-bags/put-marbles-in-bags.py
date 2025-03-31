53c8f7d0aa77af925c79cda0afef7d104f1635c5
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-03-31 12:16:57 +0530

    Time: 131 ms (58.68%) | Memory: 30.2 MB (62.81%) - LeetSync

---
+++ 2681-put-marbles-in-bags/put-marbles-in-bags.py
@@ -0,0 +1,28 @@
+from typing import List
+
+class Solution:
+    def putMarbles(self, weights: List[int], k: int) -> int:
+        if k == 1:
+            return 0
+        split = [weights[i] + weights[i+1] for i in range(len(weights) - 1)]
+        split.sort()
+        i = k - 1
+        max_score = weights[0] + weights[-1] + sum(split[-i:])
+        min_score = weights[0] + weights[-1] + sum(split[:i])
+        return max_score - min_score
+
+
+        # Wrong answer after 10 test cases because k is not considered
+        # max_score = float('-inf')  # Start with negative infinity for max
+        # min_score = float('inf')   # Start with positive infinity for min
+        # left = weights[0]
+        # right = weights[-1]
+
+        # for i in range(len(weights) - 1):  # Use 'i' instead of 'r'
+        #     l = left + weights[i]  # Corrected left-side calculation
+        #     r = right + weights[i + 1]     # Corrected right-side calculation
+            
+        #     max_score = max(max_score, l + r)
+        #     min_score = min(min_score, l + r)
+
+        # return max_score - min_score
