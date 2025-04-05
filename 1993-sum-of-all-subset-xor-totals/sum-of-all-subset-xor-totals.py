aeb3f1c46a2b4f3d150f4f5af1de457bce16dc28
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-05 11:57:56 +0530

    Time: 11 ms (76.73%) | Memory: 17.7 MB (83.05%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.py
@@ -1,11 +1,19 @@
 class Solution:
     def subsetXORSum(self, nums: List[int]) -> int:
+        def backtrack(i, xor):
+            if i == len(nums):
+                return xor
+            return backtrack(i+1, xor^nums[i]) + backtrack(i+1, xor)
+        return backtrack(0, 0)
+        
         # optimized solution
         xor_all = 0
         for num in nums:
             xor_all |= num  # Bitwise OR of all numbers
         return xor_all * (1 << (len(nums) - 1))
 
+
+        # backtrack
         res = 0
         n = len(nums)
         def backtrack(i, xor):
