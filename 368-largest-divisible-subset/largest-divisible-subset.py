ce57c4ad1a85dcfe6f9256e7e6be0e6e58c60422
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-06 21:29:15 +0530

    Time: 221 ms (22.53%) | Memory: 18.9 MB (13.58%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,5 +1,30 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        # optimized Memoization
+        nums.sort()
+        cache = {}
+
+        def dfs(i):
+            if i == len(nums):
+                return []
+            if i in cache:
+                return cache[i]
+            res = [nums[i]]
+            for j in range(i+1, len(nums)):
+                if nums[j] % nums[i] == 0:
+                    tmp = [nums[i]] + dfs(j)
+                    if len(tmp) > len(res):
+                        res = tmp
+            cache[i] = res
+            return cache[i]
+        res = []
+        for i in range(len(nums)):
+            tmp = dfs(i)
+            if len(tmp) > len(res):
+                res = tmp
+        return res
+
+        # Memoization
         nums.sort()
         cache = {}
         def backtrack(i, prev):
