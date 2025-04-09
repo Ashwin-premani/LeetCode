62ad5e327c2caf0fcd21380d1f429cc06d54e1e4
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-10 00:09:12 +0530

    Time: 107 ms (99.55%) | Memory: 27.8 MB (76.58%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,8 +1,25 @@
-from typing import List
-from functools import lru_cache
-
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
+        # tabulation
+        MOD = 10**9 + 7
+        nums.sort()
+        n = len(nums)
+
+        pow2 = [1] * n
+        for i in range(1, n):
+            pow2[i] = (pow2[i-1] * 2) % MOD
+        res = 0
+        left, right = 0, n - 1
+        while left <= right:
+            if nums[left] + nums[right] <= target:
+                res = (res + pow2[right - left]) % MOD
+                left += 1
+            else:
+                right -= 1
+
+        return  res
+
+        # Memoization
         MOD = 10**9 + 7
         nums.sort()
         n = len(nums)
