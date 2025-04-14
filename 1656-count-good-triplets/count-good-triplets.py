4227342c164fc5269161548ed4b3dd5c7e254ac8
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-14 16:06:55 +0530

    Time: 167 ms (95.30%) | Memory: 18 MB (24.73%) - LeetSync

--- 1656-count-good-triplets/count-good-triplets.py
+++ 1656-count-good-triplets/count-good-triplets.py
@@ -1,14 +1,30 @@
-from typing import List
-
 class Solution:
     def countGoodTriplets(self, arr: List[int], a: int, b: int, c: int) -> int:
         res = 0
         n = len(arr)
-        for i in range(n-2):
-            for j in range(i+1, n-1):
-                for k in range(j+1, n):
-                    if (abs(arr[i] - arr[j]) <= a and 
-                        abs(arr[j] - arr[k]) <= b and 
-                        abs(arr[i] - arr[k]) <= c):
-                        res += 1
+        pre_cnt = [0] * 1001
+        for j in range(n-1):
+            for k in range(j+1, n):
+                if abs(arr[j] - arr[k]) <= b:
+                    r = min(arr[j] + a, arr[k] + c)
+                    l = max(arr[j] - a, arr[k] - c)
+                    l = max(l, 0)
+                    r = min(r, 1000)
+                    if l <= r:
+                        res += pre_cnt[r] - (0 if l == 0 else pre_cnt[l-1])
+            for i in range(arr[j], 1001):
+                pre_cnt[i] += 1
         return res
+
+
+        # Brute Force
+        # res = 0
+        # n = len(arr)
+        # for i in range(n-2):
+        #     for j in range(i+1, n-1):
+        #         for k in range(j+1, n):
+        #             if (abs(arr[i] - arr[j]) <= a and 
+        #                 abs(arr[j] - arr[k]) <= b and 
+        #                 abs(arr[i] - arr[k]) <= c):
+        #                 res += 1
+        # return res
