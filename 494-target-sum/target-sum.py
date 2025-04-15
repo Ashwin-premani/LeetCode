d783fd16593227a811f7066ddbf36724990e5c57
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-15 15:34:01 +0530

    Time: 203 ms (28.98%) | Memory: 38.3 MB (35.93%) - LeetSync

--- 494-target-sum/target-sum.py
+++ 494-target-sum/target-sum.py
@@ -1,15 +1,44 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        # optimized bottom up
-        dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+        # Memoization
+        dp = {}
+        def f(i, cur):
+            if (i, cur) in dp:
+                return dp[(i, cur)]
+            if i == len(nums):
+                return 1 if cur == target else 0
 
-        dp[0][0] = 1 # (elements, sum) -> 1 way (1 way to sum to zero with first 0 elements)
+            s = f(i+1, cur - nums[i])
+            a = f(i+1, cur + nums[i])
+            dp[(i, cur)] = a + s
+            return dp[(i, cur)]
+        return f(0, 0)
+
+
+
+        # optimized bottom up and space
+        dp = defaultdict(int)
+
+        dp[0] = 1 # (sum) -> 1 way (1 way to sum to zero with first 0 elements)
 
         for i in range(len(nums)):
-            for cur_sum, count in dp[i].items():
-                dp[i + 1][cur_sum + nums[i]] += count
-                dp[i + 1][cur_sum - nums[i]] += count
-        return dp[len(nums)][target]
+            next = defaultdict(int)
+            for cur_sum, count in dp.items():
+                next[cur_sum + nums[i]] += count
+                next[cur_sum - nums[i]] += count
+            dp = next
+        return dp[target]
+
+        # # optimized bottom up
+        # dp = [defaultdict(int) for _ in range(len(nums) + 1)]
+
+        # dp[0][0] = 1 # (elements, sum) -> 1 way (1 way to sum to zero with first 0 elements)
+
+        # for i in range(len(nums)):
+        #     for cur_sum, count in dp[i].items():
+        #         dp[i + 1][cur_sum + nums[i]] += count
+        #         dp[i + 1][cur_sum - nums[i]] += count
+        # return dp[len(nums)][target]
 
 
         # backtrack using hash O(n * m)
