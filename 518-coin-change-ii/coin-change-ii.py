aad6236d815f930bda6fa2660a4964b7a69548bb
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-15 16:14:57 +0530

    Time: 333 ms (43.42%) | Memory: 62 MB (25.81%) - LeetSync

--- 518-coin-change-ii/coin-change-ii.py
+++ 518-coin-change-ii/coin-change-ii.py
@@ -1,7 +1,27 @@
-from typing import List
-
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
+        # tabulation
+        n = len(coins)
+        dp = [[0] * (amount + 1) for _ in range(n)]
+        for i in range(n):
+            dp[i][0] = 1
+        
+        for a in range(amount + 1):
+            if a % coins[0] == 0:
+                dp[0][a] = 1
+
+        for i in range(1,n):
+            for a in range(amount + 1):
+                not_take = dp[i - 1][a]
+                take = 0
+                if coins[i] <= a:
+                    take = dp[i][a - coins[i]]
+                dp[i][a] = take + not_take
+        return dp[n-1][amount]
+
+
+
+        # Memoization
         n = len(coins)
         dp = {}
 
