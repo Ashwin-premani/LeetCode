5dee9e57b610833e35a678486b612058b665bc97
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-16 15:32:13 +0530

    Time: 123 ms (68.93%) | Memory: 34.4 MB (82.85%) - LeetSync

--- 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
+++ 2626-count-the-number-of-good-subarrays/count-the-number-of-good-subarrays.py
@@ -1,11 +1,30 @@
 class Solution:
     def countGood(self, nums: List[int], k: int) -> int:
+        l = 0
+        n = len(nums)
+        pairs = 0
+        res = 0
+        map = defaultdict(int)
+        for r in range(n):
+            pairs += map[nums[r]]
+            map[nums[r]] += 1
+
+            while pairs >= k:
+                res += (n - r)
+                map[nums[l]] -= 1
+                pairs -= map[nums[l]]
+                l += 1
+        return res
+
+
+        #using hash
         n = len(nums)
         hash = defaultdict(int)
         l = 0
         res = 0
         pairs = 0
         for r in range(n):
+            # pairs with numbers of occurence of nums[r]
             pairs += hash[nums[r]]
             hash[nums[r]] += 1
 
