be2d24f5c10214be1c59260890be4cc56f890bb2
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-17 00:30:28 +0530

    Time: 1242 ms (26.51%) | Memory: 21.1 MB (42.05%) - LeetSync

--- 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
+++ 1669-minimum-cost-to-cut-a-stick/minimum-cost-to-cut-a-stick.py
@@ -1,13 +1,34 @@
 class Solution:
     def minCost(self, n: int, cuts: List[int]) -> int:
-        # Add start and end of stick
         cuts = [0] + sorted(cuts) + [n]
         c = len(cuts)
 
-        # Memoization table
+        dp = {}
+
+        def dfs(i, j):
+            if j - i <= 1:  # no space to cut
+                return 0
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            min_cost = float('inf')
+            for k in range(i + 1, j):
+                cost = cuts[j] - cuts[i] + dfs(i, k) + dfs(k, j)
+                min_cost = min(min_cost, cost)
+
+            dp[(i, j)] = min_cost
+            return min_cost
+
+        return dfs(0, c - 1)
+
+
+
+        # Tabulation
+        cuts = [0] + sorted(cuts) + [n]
+        c = len(cuts)
+
         dp = [[0] * c for _ in range(c)]
 
-        # Bottom-up DP
         for length in range(2, c):  # segment length
             for i in range(c - length):
                 j = i + length
