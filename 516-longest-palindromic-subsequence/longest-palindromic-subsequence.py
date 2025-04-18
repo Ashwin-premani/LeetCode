c387c2e989cd50d636b20a2baf4a4a37d5b1fb51
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-18 16:47:38 +0530

    Time: 2059 ms (5.69%) | Memory: 531.4 MB (7.63%) - LeetSync

---
+++ 516-longest-palindromic-subsequence/longest-palindromic-subsequence.py
@@ -0,0 +1,35 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+            dp = {}
+            def f(i, j):
+                if i < 0 or j < 0:
+                    return 0
+                if (i, j) in dp:
+                    return dp[(i, j)]
+                if text1[i] == text2[j]:
+                    dp[(i, j)] = 1 + f(i-1, j-1)
+                    return dp[(i, j)]
+                else:
+                    dp[(i, j)] = max(f(i-1, j), f(i, j-1))
+                    return dp[(i, j)]
+                
+            return f(len(text1)-1, len(text2) - 1)
+        t = s[::-1]
+        return longestCommonSubsequence(self, s, t)
+        
+        
+        def is_palindrome(a):
+            i = 0
+            j = len(a) - 1
+            while i <= j:
+                if a[i] != a[j]:
+                    return False
+                i += 1
+                j += 1
+            return True
+
+        def f(i):
+            if i < 0:
+                return 0
+            
