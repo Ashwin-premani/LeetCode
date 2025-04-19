1a03fc8e0bef35832b17b097ca8b5aa844830f8d
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-19 16:11:48 +0530

    Time: 63 ms (63.78%) | Memory: 18.1 MB (92.39%) - LeetSync

--- 72-edit-distance/edit-distance.py
+++ 72-edit-distance/edit-distance.py
@@ -1,5 +1,26 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
+        # Tabulation space optimization
+        n = len(word1)
+        m = len(word2)
+        prev = [j for j in range(m + 1)]
+
+        cur = [0] * (m + 1)
+        for i in range(1, n+1):
+            cur[0] = i
+            for j in range(1, m+1):
+                if word1[i-1] == word2[j-1]:
+                    cur[j] = prev[j-1]
+                else:
+                    replace = 1 + prev[j-1]
+                    insert = 1 + cur[j-1]
+                    delete = 1 + prev[j]
+                    cur[j]  = min(replace, insert, delete)
+            prev = cur.copy()
+        return prev[m]
+
+        
+        # Tabulation
         n = len(word1)
         m = len(word2)
         dp = [[0] * (m+1) for _ in range(n+1)]
