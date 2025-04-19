6ab2b3e789f3b62590f48e989fe07cdcbfec34fb
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-19 12:24:47 +0530

    Time: 219 ms (8.54%) | Memory: 17.8 MB (96.23%) - LeetSync

---
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.py
@@ -0,0 +1,20 @@
+class Solution:
+    def minDistance(self, word1: str, word2: str) -> int:
+        def longestPalindromeSubseq(self, s: str, t: str) -> int:
+            def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+                n = len(text1)
+                m = len(text2)
+                
+                prev = [0] * (m + 1)
+                for i in range(1, n + 1):
+                    cur = [0] * (m+1)
+                    for j in range(1, m + 1):
+                        if text1[i - 1] == text2[j - 1]:
+                            cur[j] = 1 + prev[j - 1]
+                        else:
+                            cur[j] = max(prev[j], cur[j - 1])
+                    prev = cur.copy()
+                            
+                return prev[m]
+            return longestCommonSubsequence(self, s, t)
+        return (len(word1) - longestPalindromeSubseq(self, word1, word2)) + (len(word2) - longestPalindromeSubseq(self, word1, word2)) 
