9b565620bc9c68bb1e2909f5eafb4472bcac87f0
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-19 12:55:34 +0530

    Time: 389 ms (92.25%) | Memory: 42.8 MB (64.55%) - LeetSync

--- 1170-shortest-common-supersequence/shortest-common-supersequence.py
+++ 1170-shortest-common-supersequence/shortest-common-supersequence.py
@@ -3,6 +3,44 @@ class Solution:
         n = len(str1)
         m = len(str2)
         
+        # Build LCS DP table
+        dp = [[0] * (m + 1) for _ in range(n + 1)]
+        for i in range(1, n + 1):
+            for j in range(1, m + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    dp[i][j] = 1 + dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+        
+        # Reconstruct SCS using string only
+        i, j = n, m
+        result = ""
+        while i > 0 and j > 0:
+            if str1[i - 1] == str2[j - 1]:
+                result = str1[i - 1] + result
+                i -= 1
+                j -= 1
+            elif dp[i - 1][j] > dp[i][j - 1]:
+                result = str1[i - 1] + result
+                i -= 1
+            else:
+                result = str2[j - 1] + result
+                j -= 1
+        
+        # Add remaining characters
+        while i > 0:
+            result = str1[i - 1] + result
+            i -= 1
+        while j > 0:
+            result = str2[j - 1] + result
+            j -= 1
+        
+        return result
+
+        # using array
+        n = len(str1)
+        m = len(str2)
+        
         dp = [[0] * (m + 1) for _ in range(n + 1)]
         for i in range(1, n + 1):
             for j in range(1, m + 1):
