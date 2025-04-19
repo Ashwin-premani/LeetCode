ad3e5219b07a6bd5d159dd1acfd1a271b6fdc84d
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-20 00:35:11 +0530

    Time: 335 ms (72.91%) | Memory: 18 MB (85.07%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.py
+++ 44-wildcard-matching/wildcard-matching.py
@@ -1,5 +1,30 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
+        # Tabulation space optimization
+        n = len(s)
+        m = len(p)
+        prev = [False] * (m+1)
+        prev[0] = True
+        for j in range(1, m + 1):
+            if p[j - 1] == '*':
+                prev[j] = prev[j - 1]
+            else:
+                prev[j] = False
+                
+        for i in range(1, n+1):
+            cur = [False] * (m+1)
+            cur[0] = False
+            for j in range(1, m+1):
+                if s[i-1] == p[j-1] or p[j-1] == '?':
+                    cur[j] = prev[j-1]
+                elif p[j-1] == '*':
+                    cur[j] = prev[j] or cur[j-1]
+                else:
+                    cur[j] = False
+            prev = cur.copy()
+                
+        return prev[m]
+
         # Tabulation
         n = len(s)
         m = len(p)
@@ -10,7 +35,7 @@ class Solution:
                 dp[0][j] = dp[0][j - 1]
             else:
                 dp[0][j] = False
-                
+
         for i in range(1, n+1):
             for j in range(1, m+1):
                 if s[i-1] == p[j-1] or p[j-1] == '?':
