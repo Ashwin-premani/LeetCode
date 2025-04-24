1164eb93c4cf00776e986c1dab10edfa2d8f2e17
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-24 13:32:47 +0530

    Time: 96 ms (75.17%) | Memory: 23.4 MB (94.87%) - LeetSync

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/best-time-to-buy-and-sell-stock-with-transaction-fee.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int], fee: int) -> int:
+        # Tabulation using 4 vars
+        n = len(prices)
+        ahead_buy = 0
+        ahead_not_buy = 0
+        cur_buy = 0
+        cur_not_buy = 0
+
+        for i in range(n - 1, -1, -1):
+            cur_buy = max(-prices[i] + ahead_not_buy, ahead_buy)
+            cur_not_buy = max(prices[i] + ahead_buy - fee, ahead_not_buy)
+            
+            ahead_buy = cur_buy
+            ahead_not_buy = cur_not_buy
+
+        return ahead_buy
+        # tabulation with 2 list(1x2) or 4 variables
         n = len(prices)
         ahead = [0] * 2
         cur = [0] * 2
