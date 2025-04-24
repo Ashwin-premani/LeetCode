4a8e5e280cec84236503975f5f35f282295a5dbc
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-24 11:26:38 +0530

    Time: 40 ms (5.07%) | Memory: 27.8 MB (5.57%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.py
@@ -1,5 +1,26 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        dp = {}
+        def f(i, holding):
+            if i == len(prices):
+                return 0
+            if (i, holding) in dp:
+                return dp[(i, holding)]
+            if holding:
+                sell = prices[i] + f(i + 1, False)
+                skip = f(i + 1, True)
+                dp[(i, holding)] = max(sell, skip)
+                return dp[(i, holding)]
+            else:
+                buy = -prices[i] + f(i + 1, True)
+                skip = f(i + 1, False)
+                dp[(i, holding)] = max(buy, skip)
+                return dp[(i, holding)]
+
+        return f(0, False)
+
+
+        # Linear
         profit = 0
         for i in range(1,len(prices)):
             if prices[i] > prices[i - 1]:
