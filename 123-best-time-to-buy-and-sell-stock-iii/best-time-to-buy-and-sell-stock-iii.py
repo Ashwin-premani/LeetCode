af1ff133ef3a5d46cb86870460d526d54e9fc092
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-24 12:32:46 +0530

    Time: 2148 ms (5.03%) | Memory: 619.9 MB (5.07%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.py
@@ -1,5 +1,24 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        # Tabulation
+
+
+        # LRu cache
+        @lru_cache(maxsize=None)
+        def f(i, holding, t):
+            if t == 2 or i == len(prices):
+                return 0
+            if holding:
+                sell = prices[i] + f(i + 1, 0, t + 1)
+                skip = f(i + 1, 1, t)
+                return max(sell, skip)
+            else:
+                buy = -prices[i] + f(i + 1, 1, t)
+                skip = f(i + 1, 0, t)
+                return max(buy, skip)
+
+        return f(0, 0, 0)
+        # Memoization
         dp = {}
         def f(i, holding, t):
             if t == 2:
