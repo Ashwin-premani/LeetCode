892fe6c0cb48cd2defc3ff57cbf3b532a3f2de69
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-24 13:25:15 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (98.12%) - LeetSync

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/best-time-to-buy-and-sell-stock-with-cooldown.py
@@ -1,5 +1,29 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        n = len(prices)
+        if n == 0:
+            return 0
+        
+        ahead = [0, 0] 
+        after_cooldown = 0 
+        
+        for i in range(n - 1, -1, -1):
+            cur = [0, 0]
+            buy = -prices[i] + ahead[1]
+            skip = ahead[0]
+            cur[0] = max(buy, skip)
+            
+            sell = prices[i] + after_cooldown
+            skip = ahead[1]
+            cur[1] = max(sell, skip)
+
+            after_cooldown = ahead[0]
+            ahead = cur[:]
+        
+        return ahead[0]
+
+
+        # Memoization
         dp = {}
         def f(i, holding):
             if i >= len(prices):
