11cd79aa2b077167f2796587dcb8400dfdbeb9ba
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-04-28 15:16:39 +0530

    Time: 175 ms (85.66%) | Memory: 18 MB (73.28%) - LeetSync

--- 368-largest-divisible-subset/largest-divisible-subset.py
+++ 368-largest-divisible-subset/largest-divisible-subset.py
@@ -1,20 +1,30 @@
 class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
-        nums.sort()
-        cache = {}
-        def backtrack(i, prev):
-            if i == len(nums):
-                return []
-            if (i, prev) in cache:
-                return cache[(i, prev)]
-            
-            res = backtrack(i+1, prev)
-            if nums[i] % prev == 0:
-                tmp = [nums[i]] + backtrack(i+1, nums[i])
-                res = tmp if len(tmp) > len(res) else res
-            cache[(i, prev)] = res
-            return res
-        return backtrack(0, 1)
+        # Tabulation
+        if not nums:
+            return []
+        nums.sort() 
+        n = len(nums)
+        dp = [1] * n 
+        prev = [-1] * n 
+
+        max_idx = 0
+        for i in range(n):
+            for j in range(i):
+                if nums[i] % nums[j] == 0:
+                    if dp[j] + 1 > dp[i]:
+                        dp[i] = dp[j] + 1
+                        prev[i] = j
+            if dp[i] > dp[max_idx]:
+                max_idx = i
+        ans = []
+        while max_idx != -1:
+            ans.append(nums[max_idx])
+            max_idx = prev[max_idx]
+
+        ans.reverse()
+        return ans
+
         # optimized Memoization
         nums.sort()
         cache = {}
