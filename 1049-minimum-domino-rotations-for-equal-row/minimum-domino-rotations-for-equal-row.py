a229926f90fa96086c3e2c79da321869c02497dd
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-05-03 11:44:18 +0530

    Time: 31 ms (76.58%) | Memory: 18.5 MB (66.24%) - LeetSync

--- 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
+++ 1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.py
@@ -1,5 +1,23 @@
 class Solution:
     def minDominoRotations(self, tops: List[int], bottoms: List[int]) -> int:
+        # Now to optimize this instead of checking each number we can check twice that is for tops[0] and bottoms[0] making it O(2n) ~ O(n) (small optimization from last)
+        def check(num):
+            top = 0
+            bottom = 0
+            for i in range(len(tops)):
+                if tops[i] != num and bottoms[i] != num:
+                    return float('inf')
+                elif tops[i] == num and bottoms[i] != num:
+                    bottom += 1
+                elif tops[i] != num and bottoms[i] == num:
+                    top += 1
+            return min(top, bottom)
+        res = min(check(tops[0]), check(bottoms[0]))
+        return res if res != float('inf') else -1
+        
+
+        # brute Force (trying every number from 1 to 6 and taking min and valid)
+        # O(6n) ~ O(n)
         res = float('inf')
         for i in range(1, 7):
             top = 0
