29a61cb3d348411f81ae16ece618e9f45d1a2234
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-05-06 12:13:27 +0530

    Time: 0 ms (100.00%) | Memory: 17.8 MB (97.22%) - LeetSync

--- 2048-build-array-from-permutation/build-array-from-permutation.py
+++ 2048-build-array-from-permutation/build-array-from-permutation.py
@@ -1,5 +1,14 @@
 class Solution:
     def buildArray(self, nums: List[int]) -> List[int]:
+        # Linear using n space
+        res = [0] * len(nums)
+        for i in range(len(nums)):
+            # res.append(nums[nums[i]])
+            res[i] = nums[nums[i]]
+        return res
+
+        # linear using no extra space but 2 loops and less optimized
+        # When you do nums[i] += n * (nums[nums[i]] % n), you’re increasing the value of nums[i]. For example, if n = 1000 and nums[i] = 999, you're storing numbers up to ~999999, which take more bytes per integer in memory.
         n = len(nums)
         for i in range(n):
             nums[i] += n * (nums[nums[i]] % n)
@@ -8,10 +17,3 @@ class Solution:
             nums[i] //= n
             
         return nums
-
-        # Linear using n space
-        res = [0] * len(nums)
-        for i in range(len(nums)):
-            # res.append(nums[nums[i]])
-            res[i] = nums[nums[i]]
-        return res
