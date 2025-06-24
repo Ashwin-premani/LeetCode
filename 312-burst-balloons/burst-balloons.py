0e9ed462eaaaa426bdada63bd7550d65425ffc71
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-06-24 22:15:16 +0530

    Time: 2844 ms (75.43%) | Memory: 20.3 MB (74.10%) - LeetSync

--- 312-burst-balloons/burst-balloons.py
+++ 312-burst-balloons/burst-balloons.py
@@ -1,5 +1,21 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
+        # Tabulation
+        nums.insert(0,1)
+        nums.append(1)
+        n = len(nums)
+        dp = [[0] * (n) for i in range(n)]
+        for i in range(n - 2, 0, -1):
+            for j in range(i, n - 1):
+                maxi = float('-inf')
+                for ind in range(i, j + 1):  
+                    cost = nums[i - 1] * nums[ind] * nums[j + 1] + dp[i][ind - 1] + dp[ind + 1][j]
+                    maxi = max(maxi, cost)
+                dp[i][j] = maxi
+        return dp[1][n-2]
+
+
+        # Memoization
         nums.insert(0,1)
         nums.append(1)
         n = len(nums)
