fe4d3e747ac5529f72a12cffd3c7429904e2af9e
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-06-26 22:20:02 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (31.71%) - LeetSync

---
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/longest-binary-subsequence-less-than-or-equal-to-k.py
@@ -0,0 +1,82 @@
+class Solution:
+    def longestSubsequence(self, s: str, k: int) -> int:
+        n = len(s)
+        count_zeros = s.count('0')
+        res = count_zeros  
+
+        val = 0
+        power = 0
+
+        for i in range(n - 1, -1, -1):
+            if s[i] == '1':
+                if power < 32: 
+                    val += 1 << power
+                    if val <= k:
+                        res += 1
+                    else:
+                        break
+                power += 1
+            else:
+                power += 1
+
+        return res
+
+        # MLE
+        n = len(s)
+        dp = [[-1] * (k + 2) for _ in range(n + 1)]
+        
+        def f(i, val):
+            if i == n:
+                return 0
+            if val > k:
+                return 0
+            if dp[i][val] != -1:
+                return dp[i][val]
+            
+            not_take = f(i + 1, val)
+            
+            new_val = val * 2 + int(s[i])
+            take = 0
+            if new_val <= k:
+                take = 1 + f(i + 1, new_val)
+            
+            dp[i][val] = max(take, not_take)
+            return dp[i][val]
+        
+        return f(0, 0)
+        
+       
+
+
+        # TLE since we are using str
+        n = len(s)
+        def bin_dec(a):
+            dec = 0
+            power = len(a) - 1
+            for bin in a:
+                dec += int(bin) * (2**power)
+                power -= 1
+            return dec
+        @lru_cache(None)
+        def f(cur, i):
+            if  i == n:
+                return len(cur) if bin_dec(cur) <= k else 0
+            not_take = f(cur, i+1)
+            take = f(cur + s[i], i + 1)
+            return max(take, not_take)
+        return f("", 0)
+        
+        
+        #TLE
+        dp = {}
+        def f(cur, i):
+            if  i == n:
+                return len(cur) if bin_dec(cur) <= k else 0
+            if (i, cur) in dp:
+                return dp[(i, cur)]
+            not_take = f(cur, i+1)
+            take = f(cur + s[i], i + 1)
+            dp[(i, cur)] =  max(take, not_take)
+            return dp[(i, cur)]
+        return f("", 0)
+            
