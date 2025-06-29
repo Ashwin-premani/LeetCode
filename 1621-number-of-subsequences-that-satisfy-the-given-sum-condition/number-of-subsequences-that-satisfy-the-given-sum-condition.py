8045dd2d0c53f6779488eca7b2f2c6e21bb74268
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-06-29 16:02:54 +0530

    Time: 315 ms (52.88%) | Memory: 153.4 MB (5.10%) - LeetSync

--- 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.py
+++ 1621-number-of-subsequences-that-satisfy-the-given-sum-condition/number-of-subsequences-that-satisfy-the-given-sum-condition.py
@@ -1,24 +1,5 @@
 class Solution:
     def numSubseq(self, nums: List[int], target: int) -> int:
-        # tabulation
-        MOD = 10**9 + 7
-        nums.sort()
-        n = len(nums)
-
-        pow2 = [1] * n
-        for i in range(1, n):
-            pow2[i] = (pow2[i-1] * 2) % MOD
-        res = 0
-        left, right = 0, n - 1
-        while left <= right:
-            if nums[left] + nums[right] <= target:
-                res = (res + pow2[right - left]) % MOD
-                left += 1
-            else:
-                right -= 1
-
-        return  res
-
         # Memoization
         MOD = 10**9 + 7
         nums.sort()
@@ -36,3 +17,25 @@ class Solution:
                 return count % MOD
 
         return dfs(0, n - 1)
+        
+        # every suseq will take 2^n
+        # uing 2 pointer
+        MOD = 10**9 + 7
+
+        n = len(nums)
+        nums.sort()
+
+        
+        pow2 = [1] * n
+        for i in range(1, n):
+            pow2[i] = (pow2[i-1] * 2) % MOD
+        l = 0
+        r = n - 1
+        res = 0
+        while l <= r:
+            if nums[l] + nums[r] <= target:
+                res += (res + pow2[r - l]) % MOD
+                l += 1
+            else:
+                r -= 1
+        return res
