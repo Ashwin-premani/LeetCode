a103e8ef84a1d8591ce1a05cdd0879cf2c849899
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-07-06 23:41:48 +0530

    Time: 298 ms (44.52%) | Memory: 48.9 MB (36.50%) - LeetSync

--- 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
+++ 1995-finding-pairs-with-a-certain-sum/finding-pairs-with-a-certain-sum.py
@@ -6,9 +6,15 @@ class FindSumPairs:
         self.cnt = Counter(nums2)
 
     def add(self, index: int, val: int) -> None:
-        self.cnt[self.nums2[index]] -= 1
-        self.nums2[index] += val
-        self.cnt[self.nums2[index]] += 1
+        old_val = self.nums2[index]
+        new_val = old_val + val
+
+        self.cnt[old_val] -= 1
+        if self.cnt[old_val] == 0:
+            del self.cnt[old_val]
+
+        self.cnt[new_val] += 1
+        self.nums2[index] = new_val
 
     def count(self, tot: int) -> int:
         pairs = 0
@@ -16,6 +22,24 @@ class FindSumPairs:
             complement = tot - i
             pairs += self.cnt.get(complement, 0)
         return pairs
+
+
+    # def __init__(self, nums1: List[int], nums2: List[int]):
+    #     self.nums1 = nums1
+    #     self.nums2 = nums2
+    #     self.cnt = Counter(nums2)
+
+    # def add(self, index: int, val: int) -> None:
+    #     self.cnt[self.nums2[index]] -= 1
+    #     self.nums2[index] += val
+    #     self.cnt[self.nums2[index]] += 1
+
+    # def count(self, tot: int) -> int:
+    #     pairs = 0
+    #     for i in self.nums1:
+    #         complement = tot - i
+    #         pairs += self.cnt.get(complement, 0)
+    #     return pairs
 
 # Your FindSumPairs object will be instantiated and called as such:
 # obj = FindSumPairs(nums1, nums2)
