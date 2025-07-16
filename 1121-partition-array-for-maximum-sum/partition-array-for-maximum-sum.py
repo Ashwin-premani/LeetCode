5fb5f0913ec32716a839b0348e8a62de21a9390c
Ashwin premani <128575470+Ashwin-premani@users.noreply.github.com>
2025-07-16 14:58:25 +0530

    Time: 235 ms (93.78%) | Memory: 17.8 MB (89.47%) - LeetSync

--- 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
+++ 1121-partition-array-for-maximum-sum/partition-array-for-maximum-sum.py
@@ -1,5 +1,22 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        # Tabulation
+        n = len(arr)
+        dp = [-1] * (n+1)
+        dp[n] = 0
+        res = 0
+        for i in range(n-1, -1 ,-1):
+            l = 0
+            maxi = float('-inf')
+            for j in range(i, min(n, i+k)):
+                l += 1
+                maxi = max(maxi, arr[j])
+                s = (l * maxi) + dp[j+1]
+                res = max(s, res)
+            dp[i]  = res
+        return dp[0]
+
+        # Memoization
         n = len(arr)
         dp = [-1] * n
         def f(i):
